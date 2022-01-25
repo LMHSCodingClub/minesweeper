e2c2d4bdf7132a31e4a8e0ce97265bcdcb87ff17
reflectronic <john-tur@outlook.com>
2022-01-25 15:29:42 -0500

    Begin populating board
    Co-authored-by: mshvartsberg <mshvartsberg@users.noreply.github.com>
    Co-authored-by: adelle7 <adelle7@users.noreply.github.com>

--- src/main/java/com/lmhscodingclub/minesweeper/BoardSpace.java
+++ src/main/java/com/lmhscodingclub/minesweeper/BoardSpace.java
@@ -1,13 +1,27 @@
 package com.lmhscodingclub.minesweeper;
 
+// [BoardSpace] [BoardSpace] [BoardSpace]
+// [BoardSpace] [BoardSpace] [BoardSpace]
+// [BoardSpace] [BoardSpace] [BoardSpace]
+
+// etc.
+
+
 public class BoardSpace {
-    int touchingNumber;
+    int adjacentMines;
 
-    public BoardSpace(boolean hasMine) {
-
+    // If adjacentMines is -1, 
+    // consider this board space a mine
+    public BoardSpace(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
     }
 
-    public BoardSpace(int value) {
-        touchingNumber = value;
+    public boolean isMine() {
+        return adjacentMines == -1;
     }
+
+    public int getAdjacentMines(){
+        return adjacentMines;
+    }
+    
 }
--- src/main/java/com/lmhscodingclub/minesweeper/MinesweeperBoard.java
+++ src/main/java/com/lmhscodingclub/minesweeper/MinesweeperBoard.java
@@ -1,10 +1,48 @@
 package com.lmhscodingclub.minesweeper;
 
 public class MinesweeperBoard {
-    private boolean shouldSpaceHaveMine() {
-        int value = (int)(Math.random()*(5))+1;
+    BoardSpace[][] boardSpaces;
+    public MinesweeperBoard(int rows, int columns) {
+        boardSpaces = new BoardSpace[rows][columns];
+    }
+
+    public void generateBoard(int noMineThisRow, int noMineThisColumn) {
+        for(int i=0; i< boardSpaces.length; i++){
+            for (int j=0; j< boardSpaces[i].length; j++) {
+                if(shouldSpaceHaveMine(5)) {
+                    // populate this space with a mine
+                    boardSpaces[i][j] = new BoardSpace(-1);
+                }
+            }
+        }
+        
+        int adjacentMineCount = 0;
+        for(int i = 0; i<boardSpaces.length; i++){
+            for(int j = 0; j<boardSpaces[i].length; j++){
+                if((i - 1) >= 0) {
+
+                    // if((j-1)>=0 && j+1<=length-1)
+                }
+            }
+        }
+    }
+
+    
+
+    private boolean shouldSpaceHaveMine(int difficulty) {
+        int value = (int)(Math.random()*(difficulty))+1;
         if(value==1)
             return true;
         return false;
     }
+
+
+    private boolean isSpaceMine(int rowIndex, int columnIndex) {
+        return boardSpaces[rowIndex][columnIndex].isMine();
+    }
+    
+
+    //somewhere...
+    // spaces[i].isMine(); 
+
 }
