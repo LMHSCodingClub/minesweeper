d3e1a1880ca4da88a2794a3c875621f26913cc0b
reflectronic <john-tur@outlook.com>
2022-02-08 15:55:07 -0500

    Change mine placement, begin finding numbers
    We started a console interface for printing the minsweeper board, but
    it does not work yet
    
    Co-authored-by: adelle7 <adelle7@users.noreply.github.com>
    Co-authored-by: mshvartsberg <mshvartsberg@users.noreply.github.com>

--- src/main/java/com/lmhscodingclub/minesweeper/App.java
+++ src/main/java/com/lmhscodingclub/minesweeper/App.java
@@ -4,12 +4,22 @@ package com.lmhscodingclub.minesweeper;
  * Hello world!
  */
 public class App {
-    
+
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MinesweeperBoard board = new MinesweeperBoard(10, 12);
+        board.generateBoard(3, 0, 0);
+
+        for (int x = 0; x < board.getWidth(); x++) {
+            for (int y = 0; y < board.getHeight(); y++) {
+                BoardSpace space = board.getBoardSpaceAt(x, y);
+                System.out.println(space.isMine() ? space.getAdjacentMines() : "*");
+                System.out.println("|");
+            }
+        }
     }
 }
--- src/main/java/com/lmhscodingclub/minesweeper/MinesweeperBoard.java
+++ src/main/java/com/lmhscodingclub/minesweeper/MinesweeperBoard.java
@@ -1,48 +1,84 @@
 package com.lmhscodingclub.minesweeper;
 
 public class MinesweeperBoard {
+    // Our code operates under the assumption that boardSpaces
+    // is an array of columns. Index n in boardSpaces will give
+    // you the nth column over from the origin. 0 is the leftmost
+    // column. Within each column, 0 is the topmost row.
+    //
+    // Using this system, boardSpaces[x][y] will return the space
+    // that is x spaces over and y spaces down. This is similar
+    // to a Cartesian coordinate system, except that increasing
+    // y values represent lower, not higher, points.
     BoardSpace[][] boardSpaces;
+
     public MinesweeperBoard(int rows, int columns) {
         boardSpaces = new BoardSpace[rows][columns];
     }
 
-    public void generateBoard(int noMineThisRow, int noMineThisColumn) {
-        for(int i=0; i< boardSpaces.length; i++){
-            for (int j=0; j< boardSpaces[i].length; j++) {
-                if(shouldSpaceHaveMine(5)) {
-                    // populate this space with a mine
-                    boardSpaces[i][j] = new BoardSpace(-1);
-                }
-            }
+    public void generateBoard(int numberOfMines, int emptyOnThisRow, int emptyOnThisColumn) {
+        // First, fill the board with mines. After we populate the board,
+        // we can calculate the numbers for the spaces adjacent to the mines.
+        for (int i = 0; i < numberOfMines; i++) {
+            int x;
+            int y;
+            do {
+                x = (int) Math.random() * (boardSpaces.length);
+                y = (int) Math.random() * (boardSpaces[0].length);
+            } while (boardSpaces[x][y] != null);
+
+            boardSpaces[x][y] = new BoardSpace(-1);
         }
-        
+
         int adjacentMineCount = 0;
-        for(int i = 0; i<boardSpaces.length; i++){
-            for(int j = 0; j<boardSpaces[i].length; j++){
-                if((i - 1) >= 0) {
-
-                    // if((j-1)>=0 && j+1<=length-1)
-                }
+        for (int i = 0; i < boardSpaces.length; i++) {
+            for (int j = 0; j < boardSpaces[i].length; j++) {
+                if (spaceHasMine(i - 1, j - 1))
+                    adjacentMineCount++;
+                if (spaceHasMine(i - 1, j))
+                    adjacentMineCount++;
+                if (spaceHasMine(i - 1, j + 1))
+                    adjacentMineCount++;
+                if (spaceHasMine(i, j - 1))
+                    adjacentMineCount++;
+                if (spaceHasMine(i, j + 1))
+                    adjacentMineCount++;
+                if (spaceHasMine(i + 1, j - 1))
+                    adjacentMineCount++;
+                if (spaceHasMine(i + 1, j))
+                    adjacentMineCount++;
+                if (spaceHasMine(i + 1, j + 1))
+                    adjacentMineCount++;
             }
         }
     }
 
-    
-
-    private boolean shouldSpaceHaveMine(int difficulty) {
-        int value = (int)(Math.random()*(difficulty))+1;
-        if(value==1)
-            return true;
-        return false;
+    public BoardSpace getBoardSpaceAt(int x, int y) {
+        return boardSpaces[x][y];
     }
 
-
-    private boolean isSpaceMine(int rowIndex, int columnIndex) {
-        return boardSpaces[rowIndex][columnIndex].isMine();
+    public int getHeight() {
+        // Height is the number of rows.
+        // Because all of our columns have the same amount of rows (the
+        // board is a rectangle), it does not matter which column we select,
+        // so select the first one.
+        return boardSpaces[0].length;
     }
-    
 
-    //somewhere...
-    // spaces[i].isMine(); 
+    public int getWidth() {
+        // Width is the number of columns.
+        return boardSpaces.length;
+    }
 
+    // return whether boardSpaces[x][y] is a mine
+    // however, we also make sure that using indices x and y
+    // will not raise IndexOutOfBoundsException
+    private boolean spaceHasMine(int x, int y) {
+        if (x < 0 || x >= boardSpaces.length)
+            return false;
+        else if (y < 0 || y >= boardSpaces[x].length)
+            return false;
+
+        return boardSpaces[x][y].isMine();
+    }
 }
